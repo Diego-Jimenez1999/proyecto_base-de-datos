ba90857b4e9ad6963af90bd53afe4e5e6b22d78f
PAVILION 15 <PAVILION 15@DESKTOP-P1TQ1VA>
2021-05-11 21:27:36 -0500

    Modifique actualizar para evitar que se abriera con el campo vacio, modifique borrar para quitar las opciones

--- src/proyecto_base_d_datos/delete_data.java
+++ src/proyecto_base_d_datos/delete_data.java
@@ -12,8 +12,7 @@ import java.awt.Color;
 
 
 public class delete_data extends javax.swing.JDialog {
-
-    String tabla = null;  
+ 
     connection_mysql mysql =new connection_mysql("root","","proyecto");
     
     String color_Lnegroo = "#000000";
@@ -38,9 +37,6 @@ public class delete_data extends javax.swing.JDialog {
         if(m==1){
         jPanel1.setBackground(Color.decode("#212121"));
         jLabel6.setForeground(Color.decode("#FAFBE9"));
-         jRadioButton1.setForeground(Color.decode("#6F7383"));
-                jRadioButton2.setForeground(Color.decode("#6F7383"));
-                jRadioButton3.setForeground(Color.decode("#6F7383"));
         }
     }
 
@@ -64,9 +60,6 @@ public class delete_data extends javax.swing.JDialog {
         buttonGroup1 = new javax.swing.ButtonGroup();
         jPanel1 = new javax.swing.JPanel();
         jLabel6 = new javax.swing.JLabel();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
-        jRadioButton3 = new javax.swing.JRadioButton();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jTextFiel_BR1 = new JTextField.jTextFiel_BR();
@@ -88,40 +81,7 @@ public class delete_data extends javax.swing.JDialog {
         jLabel6.setFont(new java.awt.Font("Tahoma", 1, 32)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(0, 169, 204));
         jLabel6.setText("Eliminar Datos");
-        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(168, 23, -1, -1));
-
-        jRadioButton1.setBackground(new java.awt.Color(255, 255, 255));
-        buttonGroup1.add(jRadioButton1);
-        jRadioButton1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jRadioButton1.setText("encuesta");
-        jRadioButton1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jRadioButton1MouseClicked(evt);
-            }
-        });
-        jPanel1.add(jRadioButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(55, 80, -1, -1));
-
-        jRadioButton2.setBackground(new java.awt.Color(255, 255, 255));
-        buttonGroup1.add(jRadioButton2);
-        jRadioButton2.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jRadioButton2.setText("Usuario");
-        jRadioButton2.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jRadioButton2MouseClicked(evt);
-            }
-        });
-        jPanel1.add(jRadioButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(246, 80, -1, -1));
-
-        jRadioButton3.setBackground(new java.awt.Color(255, 255, 255));
-        buttonGroup1.add(jRadioButton3);
-        jRadioButton3.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jRadioButton3.setText("localidad");
-        jRadioButton3.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jRadioButton3MouseClicked(evt);
-            }
-        });
-        jPanel1.add(jRadioButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(419, 80, -1, -1));
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 30, -1, -1));
 
         jButton1.setBackground(new java.awt.Color(255, 255, 255));
         jButton1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
@@ -140,7 +100,7 @@ public class delete_data extends javax.swing.JDialog {
                 jButton1ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 160, 100, 30));
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 150, 100, 30));
 
         jButton2.setBackground(new java.awt.Color(255, 51, 51));
         jButton2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
@@ -166,7 +126,7 @@ public class delete_data extends javax.swing.JDialog {
                 jTextFiel_BR1MouseClicked(evt);
             }
         });
-        jPanel1.add(jTextFiel_BR1, new org.netbeans.lib.awtextra.AbsoluteConstraints(55, 121, 435, 32));
+        jPanel1.add(jTextFiel_BR1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 90, 435, 32));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -183,18 +143,6 @@ public class delete_data extends javax.swing.JDialog {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jRadioButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jRadioButton1MouseClicked
-       tabla = "encuestas";
-    }//GEN-LAST:event_jRadioButton1MouseClicked
-
-    private void jRadioButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jRadioButton2MouseClicked
-        tabla = "usuarios";
-    }//GEN-LAST:event_jRadioButton2MouseClicked
-
-    private void jRadioButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jRadioButton3MouseClicked
-        tabla = "localidad";
-    }//GEN-LAST:event_jRadioButton3MouseClicked
-
     private void jTextFielRound_BD1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFielRound_BD1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextFielRound_BD1ActionPerformed
@@ -203,7 +151,8 @@ public class delete_data extends javax.swing.JDialog {
         connection_mysql mysql =new connection_mysql("root","","proyecto");
         String delete = jTextFiel_BR1.getText();
         mysql.MySQL_connection();
-        mysql.Delete(delete, tabla);
+        mysql.Delete(delete, "encuestas");
+        this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -299,9 +248,6 @@ public class delete_data extends javax.swing.JDialog {
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
-    private javax.swing.JRadioButton jRadioButton3;
     private JTextField.jTextFiel_BR jTextFiel_BR1;
     // End of variables declaration//GEN-END:variables
 }
